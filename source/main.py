0bf6ca70b41297fde391ecfcea44a1c12f1d1da8
rheremans@sms1.local <rheremans@agtinternational.com>
2023-03-26 12:55:28 +0100

    adding Binance api functionality

--- source/main.py
+++ source/main.py
@@ -1,5 +1,7 @@
 import os
-from api.binance_api import get_actual_price
+from api.binance_api import get_actual_price, make_curl_request, makeWalletInfoRequest, get_all_open_binance_orders
 
 if __name__ == '__main__':
     print(get_actual_price('BTCUSDT'))
+    print(make_curl_request())
+    #print(get_all_open_binance_orders())
